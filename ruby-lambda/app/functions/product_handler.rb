352dda62c6e3fe1132307d10bfeb0ae9cfe0a56b
FreakLevel <eduardo.tapiam97@gmail.com>
2019-10-04 03:44:11 +0000

    refactor: privatize response

--- ruby-lambda/app/functions/product_handler.rb
+++ ruby-lambda/app/functions/product_handler.rb
@@ -1,27 +1,31 @@
 # frozen_string_literal: true
+
 require 'json'
 require 'securerandom'
 require './app/models/product'
 
 # Handler for endpoints relationated with product's class
 module Lambda
-	class ProductHandler
-		def self.edit(event:, context:)
-			params = JSON.parse(event[:body], symbolize_names: true)
-			product_params = params[:product]
-			product = Product.new
-			product.name = product_params.dig(:name)
-			product.sku = product_params.dig(:sku)
-			product.price = product_params.dig(:price)
-			product.id = SecureRandom.uuid
-			{
-				statusCode: 200,
-				body: {
-					status: 'ok',
-					product: product.serialize
-				}
-			}
-		end
-	end
-end
+  # Handler for Product
+  class ProductHandler
+    def edit(event:, context:)
+      params = JSON.parse(event[:body], symbolize_names: true)
+      product_params = params[:product]
+      product = Product.new
+      product.name = product_params.dig(:name)
+      product.sku = product_params.dig(:sku)
+      product.price = product_params.dig(:price)
+      product.id = SecureRandom.uuid
+      response(200, product: product.serialize)
+    end
 
+    private
+
+    def response(status, body)
+      {
+        statusCode: status,
+        body: body
+      }
+    end
+  end
+end
